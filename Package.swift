87215d7510dcf804a682bdc265a01904d3b5cfb6
Julian A. Fordyce <julian.fordyce@mailchimp.com>
2020-11-17 16:53:45 -0500

    Modify package

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "Mailchimp-SDK-iOS",
+    name: "MailchimpSDK",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Mailchimp-SDK-iOS",
-            targets: ["Mailchimp-SDK-iOS"]),
+            name: "MailchimpSDK",
+            targets: ["MailchimpSDK"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,13 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Mailchimp-SDK-iOS",
-            dependencies: []),
+            name: "MailchimpSDK",
+            dependencies: [],
+            resources: [.copy("Gemfile"), .copy("Gemfile.lock")]
+        )
+        ,
         .testTarget(
-            name: "Mailchimp-SDK-iOSTests",
-            dependencies: ["Mailchimp-SDK-iOS"]),
+            name: "MailchimpSDKTests",
+            dependencies: ["MailchimpSDK"]),
     ]
 )
