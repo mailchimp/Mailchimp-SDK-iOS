9a40388c72706fc193d5ebb4d1652a7ebe4ff961
Julian A. Fordyce <julian.fordyce@mailchimp.com>
2020-11-19 11:53:06 -0500

    import!

--- Sources/MailchimpSDK/MailchimpSDK/Core/Extensions/Result.swift
+++ Sources/MailchimpSDK/MailchimpSDK/Core/Extensions/Result.swift
@@ -14,6 +14,7 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 //
+import Foundation
 
 extension Result where Success == Data, Failure == APIError {
     func decoded<T: Decodable>(using decoder: JSONDecoder = .init()) throws -> T {
